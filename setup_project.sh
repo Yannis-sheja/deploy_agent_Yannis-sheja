8a9cfe048fa36c6d4d65f11ae3f1920f662ca552
yann <y.sheja@alustudent.com>
2026-06-13 15:54:28 +0200

    Adding Signal Trap

--- setup_project.sh
+++ setup_project.sh
@@ -1,6 +1,29 @@
 #!/bin/bash 
 set -e 
-cleanup() { 
+cleanup() {
+
 	echo ""
 	echo " Script Interrupted! Performing Emergency Cleanup.. "
+
+if [ -d "$PROJECT_DIR" ]; then
+	echo " Archiving Current state to: $PROJECT_DIR_archive.tar.gz"
+
+	tar -czf "$PROJECT_DIR_archive.tar.gz" "$PROJECT_DIR"
+
+	echo "Deleting Incomplete Directory: $PROJECT_DIR"
+
+	rm -rf "$PROJECT_DIR"
+
+	echo "Archive saved as: $PROJECT_DIR_archive.tar.gz"
+else
+
+echo "There is no directory to archive. Now Exiting Cleanly."
+
+fi 
+
+exit 1 
+}
+
+trap cleanup SIGINT 
+
 
