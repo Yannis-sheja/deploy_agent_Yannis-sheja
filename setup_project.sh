cf62a7943a81ab81495040dd72037cf639ec0311
yann <y.sheja@alustudent.com>
2026-06-15 15:51:04 +0200

    Second Review

--- setup_project.sh
+++ setup_project.sh
@@ -10,15 +10,15 @@ cleanup() {
 	echo " Script Interrupted! Performing Emergency Cleanup.. "
 
 if [ -d "$PROJECT_DIR" ]; then
-	echo " Archiving Current state to: $PROJECT_DIR_archive.tar.gz"
+	echo " Archiving Current state to: ${PROJECT_DIR}_archive.tar.gz"
 
-	tar -czf "$PROJECT_DIR_archive.tar.gz" "$PROJECT_DIR"
+	tar -czf "${PROJECT_DIR}_archive.tar.gz" "$PROJECT_DIR"
 
 	echo "Deleting Incomplete Directory: $PROJECT_DIR"
 
 	rm -rf "$PROJECT_DIR"
 
-	echo "Archive saved as: $PROJECT_DIR_archive.tar.gz"
+	echo "Archive saved as: ${PROJECT_DIR}_archive.tar.gz"
 else
 
 echo "There is no directory to archive. Now Exiting Cleanly."
